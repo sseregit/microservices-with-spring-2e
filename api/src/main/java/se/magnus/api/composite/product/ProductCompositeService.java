7e17a9741270ce6729f7536a7d3b652daecf99ec
sseregit <sseregit@naver.com>
2025-03-22 15:41:55 +0900

    6장 복합 서비스 API에 새 오퍼레이션 추가

--- api/src/main/java/se/magnus/api/composite/product/ProductCompositeService.java
+++ api/src/main/java/se/magnus/api/composite/product/ProductCompositeService.java
@@ -4,22 +4,47 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "ProductComposite", description = "REST API for composite product information.")
 public interface ProductCompositeService {
 
     @Operation(
-            summary = "${api.product-composite.get-composite-product.description}",
-            description = "${api.product-composite.get-composite-product.notes}"
+        summary = "${api.product-composite.get-composite-product.description}",
+        description = "${api.product-composite.get-composite-product.notes}"
     )
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "${api.responseCodes.ok.description}"),
-            @ApiResponse(responseCode = "400", description = "${api.responseCodes.badRequest.description}"),
-            @ApiResponse(responseCode = "404", description = "${api.responseCodes.notFound.description}"),
-            @ApiResponse(responseCode = "422", description = "${api.responseCodes.unprocessableEntity.description}")
+        @ApiResponse(responseCode = "200", description = "${api.responseCodes.ok.description}"),
+        @ApiResponse(responseCode = "400", description = "${api.responseCodes.badRequest.description}"),
+        @ApiResponse(responseCode = "404", description = "${api.responseCodes.notFound.description}"),
+        @ApiResponse(responseCode = "422", description = "${api.responseCodes.unprocessableEntity.description}")
     })
-    @GetMapping(value = "/product-composite/{producteId}", produces = "application/json")
-    ProductAggregate getProduct(@PathVariable int producteId);
+    @GetMapping(value = "/product-composite/{productId}", produces = "application/json")
+    ProductAggregate getProduct(@PathVariable int productId);
+
+    @Operation(
+        summary = "${api.product-composite.create-composite-product.description}",
+        description = "${api.product-composite.create-composite-product.notes}"
+    )
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "400", description = "${api.responseCodes.badRequest.description}"),
+        @ApiResponse(responseCode = "422", description = "${api.responseCodes.unprocessableEntity.description}")
+    })
+    @PostMapping(value = "/product-composite", consumes = "application/json")
+    void createProduct(@RequestBody ProductAggregate product);
+
+    @Operation(
+        summary = "${api.product-composite.delete-composite-product.description}",
+        description = "${api.product-composite.delete-composite-product.notes}"
+    )
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "400", description = "${api.responseCodes.badRequest.description}"),
+        @ApiResponse(responseCode = "422", description = "${api.responseCodes.unprocessableEntity.description}")
+    })
+    @DeleteMapping(value = "/product-composite")
+    void deleteProduct(@RequestBody ProductAggregate product);
 }
--- microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/services/ProductCompositeServiceImpl.java
+++ microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/services/ProductCompositeServiceImpl.java
@@ -1,14 +1,16 @@
 package se.magnus.microservices.composite.product.services;
 
-import org.springframework.stereotype.Component;
+import java.util.List;
 import org.springframework.web.bind.annotation.RestController;
-import se.magnus.api.composite.product.*;
+import se.magnus.api.composite.product.ProductAggregate;
+import se.magnus.api.composite.product.ProductCompositeService;
+import se.magnus.api.composite.product.RecommendationSummary;
+import se.magnus.api.composite.product.ReviewSummary;
+import se.magnus.api.composite.product.ServiceAddresses;
 import se.magnus.api.core.product.Product;
 import se.magnus.api.core.recommendation.Recommendation;
 import se.magnus.api.core.review.Review;
 import se.magnus.util.http.ServiceUtil;
-
-import java.util.List;
 
 @RestController
 public class ProductCompositeServiceImpl implements ProductCompositeService {
@@ -16,7 +18,8 @@ public class ProductCompositeServiceImpl implements ProductCompositeService {
     private final ServiceUtil serviceUtil;
     private final ProductCompositeIntegration integration;
 
-    public ProductCompositeServiceImpl(ServiceUtil serviceUtil, ProductCompositeIntegration integration) {
+    public ProductCompositeServiceImpl(ServiceUtil serviceUtil,
+        ProductCompositeIntegration integration) {
         this.serviceUtil = serviceUtil;
         this.integration = integration;
     }
@@ -28,27 +31,46 @@ public class ProductCompositeServiceImpl implements ProductCompositeService {
         List<Recommendation> recommendations = integration.getRecommendations(producteId);
         List<Review> reviews = integration.getReviews(producteId);
 
-        return createProductAggregate(product, recommendations, reviews, serviceUtil.getServiceAddress());
+        return createProductAggregate(product, recommendations, reviews,
+            serviceUtil.getServiceAddress());
     }
 
-    private ProductAggregate createProductAggregate(Product product, List<Recommendation> recommendations, List<Review> reviews, String serviceAddress) {
+    @Override
+    public void createProduct(ProductAggregate product) {
+
+    }
+
+    @Override
+    public void deleteProduct(ProductAggregate product) {
+
+    }
+
+    private ProductAggregate createProductAggregate(Product product,
+        List<Recommendation> recommendations, List<Review> reviews, String serviceAddress) {
         int productId = product.getProductId();
         String name = product.getName();
         int weight = product.getWeight();
 
-        List<RecommendationSummary> recommendationSummaries = recommendations == null ? null : recommendations.stream()
-                .map(r -> new RecommendationSummary(r.getRecommendationId(), r.getAuthor(), r.getRate()))
+        List<RecommendationSummary> recommendationSummaries =
+            recommendations == null ? null : recommendations.stream()
+                .map(r -> new RecommendationSummary(r.getRecommendationId(), r.getAuthor(),
+                    r.getRate()))
                 .toList();
 
         List<ReviewSummary> reviewSummaries = reviews == null ? null : reviews.stream()
-                .map(r -> new ReviewSummary(r.getReviewId(), r.getAuthor(), r.getSubject()))
-                .toList();
+            .map(r -> new ReviewSummary(r.getReviewId(), r.getAuthor(), r.getSubject()))
+            .toList();
 
         String productAddress = product.getServiceAddress();
-        String reviewAddress = (reviews != null && reviews.size() > 0) ? reviews.get(0).getServiceAddress() : "";
-        String recommendationAddress = (recommendations != null && recommendations.size() > 0) ? recommendations.get(0).getServiceAddress() : "";
-        ServiceAddresses serviceAddresses = new ServiceAddresses(serviceAddress, productAddress, reviewAddress, recommendationAddress);
+        String reviewAddress =
+            (reviews != null && reviews.size() > 0) ? reviews.get(0).getServiceAddress() : "";
+        String recommendationAddress =
+            (recommendations != null && recommendations.size() > 0) ? recommendations.get(0)
+                .getServiceAddress() : "";
+        ServiceAddresses serviceAddresses = new ServiceAddresses(serviceAddress, productAddress,
+            reviewAddress, recommendationAddress);
 
-        return new ProductAggregate(productId, name, weight, recommendationSummaries, reviewSummaries, serviceAddresses);
+        return new ProductAggregate(productId, name, weight, recommendationSummaries,
+            reviewSummaries, serviceAddresses);
     }
 }
