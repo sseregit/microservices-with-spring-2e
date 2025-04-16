5d44a1da5bea2ea20a4eb56b50e6180578059fd3
sseregit <sseregit@naver.com>
2025-04-16 22:14:30 +0900

    11장 검색 서비스 접근 보안 - 유레카

---
+++ spring-cloud/eureka-server/src/main/java/se/magnus/springcloud/eurekaserver/SecurityConfig.java
@@ -0,0 +1,40 @@
+package se.magnus.springcloud.eurekaserver;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Value("${app.eureka-username}")
+    private String username;
+    @Value("${app.eureka-password}")
+    private String password;
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername(username)
+            .password("{noop}" + password)
+            .authorities("USER")
+            .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+            .httpBasic(withDefaults());
+        return http.build();
+    }
+
+}
--- spring-cloud/eureka-server/src/test/java/se/magnus/springcloud/eurekaserver/EurekaServerApplicationTests.java
+++ spring-cloud/eureka-server/src/test/java/se/magnus/springcloud/eurekaserver/EurekaServerApplicationTests.java
@@ -1,13 +1,49 @@
 package se.magnus.springcloud.eurekaserver;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class EurekaServerApplicationTests {
 
     @Test
     void contextLoads() {
     }
 
+    @Value("${app.eureka-username}")
+    private String username;
+    @Value("${app.eureka-password}")
+    private String password;
+
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    public void setRestTemplate(TestRestTemplate restTemplate) {
+        this.restTemplate = restTemplate.withBasicAuth(username, password);
+    }
+
+    @Test
+    void catalogLoads() {
+        String expectedResponseBody = """
+            {"applications":{"versions__delta":"1","apps__hashcode":"","application":[]}}
+            """.strip();
+        ResponseEntity<String> entity = restTemplate.getForEntity("/eureka/apps", String.class);
+        assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(entity.getBody()).isEqualTo(expectedResponseBody);
+    }
+
+    @Test
+    void healthy() {
+        String expectedResponseBody = "{\"status\":\"UP\"}";
+        ResponseEntity<String> entity = restTemplate.getForEntity("/actuator/health", String.class);
+        assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(entity.getBody()).isEqualTo(expectedResponseBody);
+    }
 }
